d6edb412ee3cc785c594da4254dffb28fe3b8507
Zainap16 <zvanblerck1@gmail.com>
2024-04-23 20:54:08 +0200

    2022 pie chart complete

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 data = pd.read_excel("results.xlsx")
-print(data)
 
+# prepare data for pie chart
+labels = ["Unemployed","Employed"]
+sizes = [data["Unemployed"].sum(), data["Employed"].sum()]
+colors = ["lightcoral","lightskyblue"]
+
+explode = (0,0.1)
+#create pie chart
+plt.title("Unemployed VS Employement")
+plt.pie(sizes,labels=labels, autopct='%1.1f%%', startangle=140, colors=colors, explode=explode)
+plt.axis("equal")
+# Equal aspect ratio ensures that pie is drawn as a circle.
+plt.show()
 
 
 
