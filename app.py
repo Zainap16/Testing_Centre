8457e62f7809bbad73255e9437cbde18d46047f8
Shaqeel Less <54390756+MogammadShaqeelless16@users.noreply.github.com>
2024-04-24 16:09:29 +0200

    update env variables

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 import pandas as pd
+from dotenv import load_dotenv
 import matplotlib.pyplot as plt
 import psycopg2
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get database connection details from environment variables
+db_host = os.environ.get('DB_HOST')
+db_port = os.environ.get('DB_PORT')
+db_name = os.environ.get('DB_NAME')
+db_user = os.environ.get('DB_USER')
+db_password = os.environ.get('DB_PASSWORD')
 
 # Connect to the PostgreSQL database
 conn = psycopg2.connect(
-    host='dpg-cokgiiv79t8c73ca77kg-a',
-    port='5432',
-    dbname='matric',
-    user='matric_user',
-    password='YlFGQ4TUvdU6937gns6IUdflTGoDnvmk'
+    host=db_host,
+    port=db_port,
+    dbname=db_name,
+    user=db_user,
+    password=db_password
 )
 
 # Create a cursor object
